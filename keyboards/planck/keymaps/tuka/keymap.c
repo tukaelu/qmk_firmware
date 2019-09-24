e7859c2b8f8dc593b489b8bbf541e1bc22ac0d1a
tuka <tukaelu@hatena.ne.jp>
2019-09-24 12:46:44 +0900

    :hammer: change enter key

--- keyboards/planck/keymaps/tuka/keymap.c
+++ keyboards/planck/keymaps/tuka/keymap.c
@@ -44,16 +44,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  Up  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | GUI  | Ctrl | Alt  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Esc  | GUI  | Ctrl | Alt  |Lower |    Space    |Raise |Enter | Left |Right | Down |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_ESC,  KC_LGUI, KC_LCTL, KC_LALT, LT(_LOWER,KC_LANG2),   KC_SPC,  KC_SPC,  LT(_RAISE,KC_LANG1),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,
+    KC_ESC,  KC_LGUI, KC_LCTL, KC_LALT, LT(_LOWER,KC_LANG2),   KC_SPC,  KC_SPC,  LT(_RAISE,KC_LANG1),  KC_ENT, KC_LEFT, KC_RGHT, KC_DOWN
 ),
 
 /* 10Key
